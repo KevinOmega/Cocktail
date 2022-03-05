e5c89c5433f6d862aaa41a1f600bcb7993a94bb9
Kevin Huayllas <KevinOmega01@gmail.com>
2022-03-05 16:52:13 -0400

    Navbar added
    In this commit we add the all the basics funtionalities of an navbar , also animations

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [isClicked, setIsClicked] = useState(false);
   return (
-    <div>
-      <h1>NavBar page</h1>
+    <div className="navbar">
+      <div className="navbar-center">
+        <h2>Cocktail List</h2>
+      </div>
+      <nav className={`links ${isClicked && "links-active"}`}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="btn-container" onClick={() => setIsClicked(!isClicked)}>
+        <div className={`links-btn ${isClicked && "links-btn-active"}`} />
+      </div>
     </div>
   );
 };
