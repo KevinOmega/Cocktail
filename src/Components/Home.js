c5cb3d58b02049eae2f43ce04ee95be4b904e267
Kevin Huayllas <KevinOmega01@gmail.com>
2022-03-06 14:20:48 -0400

    basics features of home page has been finished
    aditional animation to navbar has been added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,14 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem
-        sequi dolore molestias, est aliquid autem hic, voluptas magni vero rem
-        quasi et adipisci, mollitia perspiciatis nemo dignissimos earum sapiente
-        alias modi aliquam error ea eum. Neque sed voluptas tempore eveniet
-        excepturi delectus rem, corrupti rerum, eum quia, doloribus quidem vel
-        magnam voluptates tempora accusantium! no
-      </h1>
+      <div className="home-center">
+        <h2 className="home-text">Home</h2>
+        <h4 className="home-text">Welcome scroll down and see our products</h4>
+      </div>
     </div>
   );
 };
