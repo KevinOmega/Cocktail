5a46f7b1715d346c1c79f305b96caeea5757d51d
Kevin Huayllas <KevinOmega01@gmail.com>
2022-03-08 12:32:41 -0400

    the principals features of products has been finished
    the nav links in the nav bar has been updated

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,13 +10,13 @@ const NavBar = () => {
       <nav className={`links ${isClicked && "links-active"}`}>
         <ul>
           <li>
-            <a href="#home">Home</a>
+            <a href="/#home">Home</a>
           </li>
           <li>
-            <a href="#products">Products</a>
+            <a href="/#products">Products</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="/#about">About</a>{" "}
           </li>
         </ul>
       </nav>
