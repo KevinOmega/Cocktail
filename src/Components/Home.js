b9ba3abd51e959f09443aeaad3fc292355af5a79
Kevin Huayllas <KevinOmega01@gmail.com>
2022-05-25 15:48:35 -0400

    mobile navbar error has been fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -15,7 +16,9 @@ const Home = () => {
             Because never is a bad time to cool off. <br></br>If you wanna see
             more of our products please click on the button below
           </p>
-          <a href="/list">See more</a>
+          <Link className="btn-link" to={"/list"}>
+            See more
+          </Link>
         </div>
       </div>
     </div>
